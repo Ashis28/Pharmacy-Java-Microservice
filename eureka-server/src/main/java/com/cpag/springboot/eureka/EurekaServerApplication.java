bb0f173861b8b6e7a075bf7c481cd488fd1c363a
Jahnavi <120454226+kjahnavi03@users.noreply.github.com>
2026-03-19 16:14:26 +0530

    Update EurekaServerApplication.java
    Dummy lines added for for testing purpose

--- eureka-server/src/main/java/com/cpag/springboot/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/cpag/springboot/eureka/EurekaServerApplication.java
@@ -11,7 +11,8 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 		System.out.println("Eureka server started at port 8761...");
-		//EXAMPLE
+		//
+		System.out.println("Jahnavi's git testing..");
 	}
 
 }
